8748ebd075dab51ddcfd7142eac1e879faa6b44e
ZhangHao20261314 <3913141387@qq.com>
2026-09-11 20:21:01 +0800

    Add files via upload

---
+++ fix_footer.py
@@ -0,0 +1,16 @@
+import re
+
+src = "/home/ubuntu/app/hugo-blog/themes/PaperMod/layouts/_partials/footer.html"
+dst = "/home/ubuntu/app/hugo-blog/layouts/_partials/footer.html"
+
+with open(src, 'r') as f:
+    content = f.read()
+
+# Remove the hardcoded Powered by Hugo & PaperMod block and its comment
+pattern = r'\{\{/\*.*?The "Powered by Hugo" text.*?\}\}\s*<span>\s*Powered by.*?</span>'
+new_content = re.sub(pattern, '', content, flags=re.DOTALL)
+
+with open(dst, 'w') as f:
+    f.write(new_content)
+
+print("Custom clean footer.html created successfully!")
