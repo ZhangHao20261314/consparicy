cc8c11eb5d90e42d4bfb7ca33991bb09822916e7
ZhangHao20261314 <3913141387@qq.com>
2026-09-11 20:21:22 +0800

    Update fix_footer.py

--- fix_footer.py
+++ fix_footer.py
@@ -6,6 +6,8 @@ dst = "/home/ubuntu/app/hugo-blog/layouts/_partials/footer.html"
 with open(src, 'r') as f:
     content = f.read()
 
+
+
 # Remove the hardcoded Powered by Hugo & PaperMod block and its comment
 pattern = r'\{\{/\*.*?The "Powered by Hugo" text.*?\}\}\s*<span>\s*Powered by.*?</span>'
 new_content = re.sub(pattern, '', content, flags=re.DOTALL)
